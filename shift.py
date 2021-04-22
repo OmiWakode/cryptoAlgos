339a0057f77516253cb5a56e37e2d0ba2f62bb27
Omi Wakode <omi.wakode@gmail.com>
2021-04-23 05:27:42 +0530

    added decryption functions

--- shift.py
+++ shift.py
@@ -1,7 +1,6 @@
 # Shift Cipher
 
-msg = input('Enter the string to be encrypted: ')
-key = int(input('Enter key: '))
+
 
 def encrypt(msg, key):
     code = [ord(x)-97 for x in msg]
@@ -14,9 +13,43 @@ def encrypt(msg, key):
     encrypt = ''.join([elem for elem in encrypt])
     print('The encrypted String is' ,encrypt)
 
+def decrypt(msg,key):
+    code = [ord(x)-97 for x in msg]
+    print('The coded string is: ', code)
+
+    ecode = [(x - key)%26 for x in code]
+    print('The dectpted code: ',ecode)
+
+    encrypt = [chr(x + 97) for x in ecode]
+    encrypt = ''.join([elem for elem in encrypt])
+    print('The decrypted String is' ,encrypt)
+
+
+while(1):
+    print('Enter 1 for encrytion')
+    print('Enter 2 for decrytion')
+    print('Enter 0 for exit')
+    
+    choice = int(input('Choice: '))
+
+    if(choice==0):
+        break
+
+    if(choice==1):
+        msg = input('Enter the string to be encrypted: ')
+        key = int(input('Enter key: '))
+        encrypt(msg,key)
+        continue
+
+    if(choice==2):
+        msg = input('Enter the string to be decrypted: ')
+        key = int(input('Enter key: '))
+        decrypt(msg,key)
+        continue
+
 
     
-encrypt(msg,key)
+
 
 
 
--- vernam.py
+++ vernam.py
@@ -1,10 +1,10 @@
 
-from operator import sub
-msg = input('Enter the string to be encrypted: ')
-key = input('Enter key: ')
 
 def addtr(a, b):
     return (a+b)%26
+
+def subtr(a, b):
+    return(a-b)%26
 
 def encrypt(msg, key):
     code = [ord(x)-97 for x in msg]
@@ -20,7 +20,41 @@ def encrypt(msg, key):
     encrypt = ''.join([elem for elem in encrypt])
     print('The encrypted String is' ,encrypt)
 
-
+def decrypt(msg, key):
+    code = [ord(x)-97 for x in msg]
+    codekey = [ord(x)-97 for x in key]
+    print('The coded string is: ', code)
+    print('The coded key is: ', codekey)
     
-encrypt(msg,key)
+
+    ecode = list(map(subtr, code, codekey))
+    print('The encypted code: ',ecode)
+
+    encrypt = [chr(x + 97) for x in ecode]
+    encrypt = ''.join([elem for elem in encrypt])
+    print('The encrypted String is' ,encrypt)
+
+
+
+while(1):
+    print('Enter 1 for encrytion')
+    print('Enter 2 for decrytion')
+    print('Enter 0 for exit')
+    
+    choice = int(input('Choice: '))
+
+    if(choice==0):
+        break
+
+    if(choice==1):
+        msg = input('Enter the string to be encrypted: ')
+        key = input('Enter key: ')
+        encrypt(msg,key)
+        continue
+
+    if(choice==2):
+        msg = input('Enter the string to be decrypted: ')
+        key = input('Enter key: ')
+        decrypt(msg,key)
+        continue
 
